238885f07bc26d20f748b68c3d3e04f7ee1f8f3c
anser-software <ansersoftware@gmail.com>
2022-02-22 23:25:09 +0200

    Finish; adjustments

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,10 +10,14 @@ public class PlayerMove : MonoBehaviour
     public bool onMovingPlatform { get; private set; }
 
     [SerializeField]
-    private Vector3 movement, jumpForce;
+    private Vector3 jumpForce;
 
     [SerializeField]
-    private float platformInFrontCheckDistanceDown, platformInFrontCheckDistanceForward, groundCheckDistance, minJumpDistance, jumpForceDelay;
+    private float speed;
+
+    [SerializeField]
+    private float platformInFrontCheckDistanceDown, platformInFrontCheckDistanceForward, 
+        groundCheckDistance, minJumpDistance, jumpForceDelay, enableFallAnimAirborneTime, jumpDecceleration, gravityStrength;
 
     [SerializeField]
     private LayerMask ground;
@@ -25,7 +29,11 @@ public class PlayerMove : MonoBehaviour
 
     private bool grounded = true;
 
-    private float jumpTimer, fallTimer;
+    private float jumpTimer, fallTimer, airborneTimer;
+
+    private Vector3 currentJumpForce = Vector3.zero;
+
+    private float currentGravity;
 
     private void Awake()
     {
@@ -48,19 +56,25 @@ public class PlayerMove : MonoBehaviour
             onMovingPlatform = hit.collider.transform.GetComponent<Platform>() != null;
 
             animator.SetTrigger("Run");
+
+            airborneTimer = 0F;
         }
         else
         {
             grounded = false;
 
+            airborneTimer += Time.deltaTime;
 
-            if(jumpTimer <= 0F && fallTimer <= 0F)
+            currentGravity += Time.deltaTime * 9.81F * gravityStrength;
+
+            if (jumpTimer <= 0F && fallTimer <= 0F && airborneTimer > enableFallAnimAirborneTime)
             {
                 Debug.Log("Not grounded; Fall");
 
                 animator.SetTrigger("Fall");
                 fallTimer = 0.8F;
             }
+
         }
 
         if (jumpTimer > 0F)
@@ -72,14 +86,15 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var nextPlatformRayOrigin = transform.position + movement.normalized;
+        var nextPlatformRayOrigin = transform.position + Vector3.right;
 
-        if (Physics.Raycast(nextPlatformRayOrigin, Vector3.down, platformInFrontCheckDistanceDown))
+        if (Physics.Raycast(nextPlatformRayOrigin, Vector3.down, platformInFrontCheckDistanceDown, ground.value))
         {
-            rb.AddForce(movement, ForceMode.Force);
+            //rb.AddForce(Vector3.right, ForceMode.Force);
+            rb.velocity = Vector3.right * speed + Vector3.down * currentGravity + currentJumpForce;
         }
 
-        var jumpRayOrigin = transform.position + movement.normalized * platformInFrontCheckDistanceForward;
+        var jumpRayOrigin = transform.position + Vector3.right * platformInFrontCheckDistanceForward;
 
         if (Physics.Raycast(jumpRayOrigin, Vector3.down, out RaycastHit hit, platformInFrontCheckDistanceDown))
         {
@@ -88,6 +103,9 @@ public class PlayerMove : MonoBehaviour
                 Jump();
             }
         }
+
+        currentJumpForce = Vector3.Lerp(currentJumpForce, Vector3.zero, Time.deltaTime * jumpDecceleration);
+        //rb.velocity = rb.velocity.normalized * speed;
     }
 
     private void Jump()
@@ -97,10 +115,15 @@ public class PlayerMove : MonoBehaviour
 
         jumpTimer = 0.6F;
 
-        DOTween.Sequence().SetDelay(jumpForceDelay).OnComplete(() => rb.AddForce(jumpForce, ForceMode.Impulse));
+        DOTween.Sequence().SetDelay(jumpForceDelay).OnComplete(() => currentJumpForce = jumpForce);//rb.AddForce(jumpForce, ForceMode.Impulse));
 
         animator.SetTrigger("Jump");
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        if(Vector3.Dot(collision.contacts[0].normal, Vector3.up) > 0.5F)
+            currentGravity = 0.2F;
+    }
 
 }
